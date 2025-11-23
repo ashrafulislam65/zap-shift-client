4d92fece28dbd68dece7d55e0ce3bcf42c8314a2
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-11-24 03:35:02 +0600

    coverage page with map implementation

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
         <li><NavLink to='/'>Home</NavLink></li>
         <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/coverage'>Coverage</NavLink></li>
         
     </>
     return (
